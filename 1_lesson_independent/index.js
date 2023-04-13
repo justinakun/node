dc2500c66f296ae6138333d470350a57e2225856
Justina Kunigonyte <justina.kunigonyte@gmail.com>
2023-04-13 21:03:51 +0300

    submitting my first individual task to git hub

---
+++ 1_lesson_independent/index.js
@@ -0,0 +1,45 @@
+const express = require("express"); 
+const app = express(); 
+const port = 3000; 
+
+const casual = require("casual");
+
+app.get("/randomUser", (req, res) => {
+    const user = {
+        name: `${casual.first_name}`,
+        surname: `${casual.last_name}`,
+        country: `${casual.country}`,
+        street: `${casual.street}`,
+        zip: `${casual.zip(digits = 5)}`,
+    }
+    res.send(user); 
+});
+
+app.get("/randomColor", (req, res) => {
+    res.send(`${casual.safe_color_name}`);
+})
+
+app.get("/randomColors", (req, res) => {
+    let colorArray = [];
+    for (let i = 0; i < 5; i++){
+        colorArray[i] = casual.safe_color_name;
+    }
+    res.send(colorArray);
+})
+
+app.get("/randomPlaces", (req, res) => {
+    let randomNumber = casual.integer(from = 1, to = 5);
+    let myObjectArray = [];
+    for(let i = 0; i < randomNumber; i++){
+        myObjectArray[i] = {
+        country: `${casual.country}`,
+        city: `${casual.city}`,
+        address: `${casual.address}`
+        }
+    }
+    res.send(myObjectArray);
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
