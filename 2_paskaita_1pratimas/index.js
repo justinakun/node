f4bbb925e967872f47d7c35aebdcbe9161a25531
Justina Kunigonyte <justina.kunigonyte@gmail.com>
2023-04-18 19:05:53 +0300

    adjustments

--- 2_paskaita_1pratimas/index.js
+++ 2_paskaita_1pratimas/index.js
@@ -1,23 +1,31 @@
-const express = require("express"); 
-const cors = require("cors");
-const app = express(); 
-const port = 3000;
+const express = require("express"); //express importas
+const cors = require("cors"); //cors importas
+const app = express(); //express aplikacijos inicijavimas
+const port = 3000; //kanalas reikalingas serveriui
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json()); //aplikacija priima duomenis JSON formatu
+app.use(cors()); //aplikacija naudoja CORS apsauga
 
 const cars = [];
 
+//GET kelias, kuris grazina duomenis
 app.get("/", (req, res) => {
+    //res (response) - duomenys kuriuos mes grazinam
     res.send(cars);
 }) 
 
-app.post("/cars", (req, res) =>{
-    const carBrand = "Audi";
-    cars.push(carBrand);
-    res.send(cars);
+app.post("/", (req, res) =>{
+    //req(request) - duomenys kuriuos mes gaunam is isores
+    //req.body - pagrindiniai duomenys is isores
+    const car = req.body.car;
+    cars.push(car);
+    res.send(req.body); //POST dalyje siunciam atgal klientui, tai ka jis pats atsiunte mum
 })
 
+//app.listen() - metodas kuris paleidzia klausytis musu serverio nurodytu kanalu
+// port - kanalas
+//() => {} - funkcija kuris pasileidzia kai serveris startuoja
+//console.log naudojam, kad zinoti kokiu kanalu paleido serveri
 app.listen(port, () =>{
-    console.log(`Server is running on the ${port}`);
+    console.log(`Server is running on the http://localhost:${port}/`);
 });
