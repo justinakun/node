938ee6bfaefb194c91f3d5b65784d4d9971977ca
Justina Kunigonyte <justina.kunigonyte@gmail.com>
2023-04-26 18:43:42 +0300

    improved the task

--- 5_paskaita_1pratimas/index.js
+++ 5_paskaita_1pratimas/index.js
@@ -13,20 +13,27 @@ const cart = [{id: 1, name:"potatoes", price: 5, quantity: 2}];
 //return the whole shopping cart
 app.get("/", (req, res) => {
     res.send(cart);
+    //galima ir res.json(cart)
+    //send yra universalus
 });
 
 //add an item to the cart
 app.post("/", (req, res)=>{
     const item = req.body;
+    // item.id = cart.length + 1; // pridedamas dinaminis id pagal krepselio ilgi + 1
     cart.push(item);
-    res.send(item);
+    res.status(201).send(item); //grazina http statusa, kuris nurodo response busena.
 })
 
 //return one item from the cart
 app.get("/cart/item/:id", (req, res)=>{
     const id = req.params.id;
     const itemById = cart.find(item=> item.id === +id)
-    res.send(itemById);
+    if(!itemById){
+        res.status(404).send("Item not found");
+    } else{
+        res.send(itemById);
+    }    
 })
 
 app.listen(port, () => console.log(`Server started on port ${port}...`));
