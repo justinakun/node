d5f305c7bcd62f3643330676bc704d7721f82a89
Justina Kunigonyte <justina.kunigonyte@gmail.com>
2023-05-15 21:33:13 +0300

    still need to go through 11th tasks

---
+++ 11_paskaita_2pratimas/index.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ObjectId } = require('mongodb');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 8080;
+const URI = process.env.DB_CONNECTION_STRING;
+const dbName = process.env.DB_NAME;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const client = new MongoClient(URI);
+
+app.get('/categories', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con.db(dbName).collection('categories').find().toArray();
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.get('/products', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con.db(dbName).collection('categories').find().toArray();
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.post('/users', async (req, res) => {
+  const user = req.body;
+  try {
+    const con = await client.connect();
+    const data = await con.db(dbName).collection('users').insertOne(user);
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.get('/comments', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con
+      .db(dbName)
+      .collection('comments')
+      .aggregate([
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'user_id',
+            foreignField: '_id',
+            as: 'user',
+          },
+        },
+        {
+          $addFields: {
+            user_id: { $toObjectId: '$user_id' },
+          },
+        },
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'user_id',
+            foreignField: '_id',
+            as: 'user',
+          },
+        },
+      ])
+      .toArray();
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.delete('/comments/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const con = await client.connect();
+    const data = await con
+      .db(dbName)
+      .collection('comments')
+      .deleteOne({ _id: new ObjectId(id) });
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on the ${port}`);
+});
